8b6a4e68274b3c1b99f03449b428071b5a75de9b
Safaizal <safaizalways@gmail.com>
2026-09-07 20:14:23 +0530

    modified

---
+++ cmus/lib.pl
@@ -0,0 +1,98 @@
+/home/faizal/Music/AnneMarie  Ed Sheeran  2002 Official Acoustic Video.mp3
+/home/faizal/Music/au_uu_SzH34yR2.mp3
+/home/faizal/Music/blooming_story.mp3
+/home/faizal/Music/Dead Mount Death Play Ending Full   Iolite (アイオライト)  by Inori Minase instrumental.mp3
+/home/faizal/Music/Dream lantern.mp3
+/home/faizal/Music/Euphoria.mp3
+/home/faizal/Music/God eater have you ever seen.mp3
+/home/faizal/Music/haruhi_suzumiya.mp3
+/home/faizal/Music/(Jian Hao) Never Falling  Amander Sings MV I am a Teacher OST.mp3
+/home/faizal/Music/left-and-right--jungkook--charlie-puth--english--remix Ringtone.mp3
+/home/faizal/Music/LINE_MOVIE_1652113935719.mp4
+/home/faizal/Music/LINE_MOVIE_1652113941752.mp4
+/home/faizal/Music/LINE_MOVIE_1652114135752.mp4
+/home/faizal/Music/LINE_MOVIE_1652114558241.mp4
+/home/faizal/Music/Nandemonaiya  movie ver.mp3
+/home/faizal/Music/Nandemonaiya (your name) movie ver.mp3
+/home/faizal/Music/Noragami Ost - Shadow Dancing.mp3
+/home/faizal/Music/[OFFICIAL AUDIO] V - Christmas Tree __ 그 해 우리는(Our Beloved Summer) OST Part.5.mp3
+/home/faizal/Music/One Time-Justin Bieber.mp3
+/home/faizal/Music/QuickSilver Saves Everyone From Exploding XMansion Scene   Sweet Dreams  XMen Apocalypse 2016.mp3
+/home/faizal/Music/Renai Circulation  Ringtone 19.mp3
+/home/faizal/Music/SAO menu.mp3
+/home/faizal/Music/Shigatsu wa Kimi no Uso Twinkle Twinkle Little Star.mp3
+/home/faizal/Music/Umareru Negai Fruits Basket Original Soundtrack OST Disc 1.mp3
+/home/faizal/Music/uncanny counter.mp3
+/home/faizal/Music/y2mate.com - Levi Oi notification sound.mp3
+/home/faizal/Music/yt1s.com - Jungkook Euphoria instrumental ringtone .mp3
+/home/faizal/Music/yt1s.com - Never Falling  Amander Sings MV I am a Teacher OST.mp3
+/home/faizal/Music/zenitsu_nezuko_chan.mp3
+/home/faizal/Music/あいみょん 空の青さを知る人よmovie ver.mp3
+/home/faizal/Music/ 好きたから .mp3
+/home/faizal/Music/好きだから.mp3
+/home/faizal/Music/感情を込めてハレハレヤ 歌ってみた verSou.mp3
+/home/faizal/Music/涼宮ハルヒの憂鬱BGM　いつもの風景.mp3
+/home/faizal/Music/涼宮ハルヒの憂鬱BGM　何かがおかしい.mp3
+/home/faizal/Music/Ed Sheeran - Galway Girl [Official Music Video] (320  kbps).mp3
+/home/faizal/Music/zenitsu_yameteeeeee.mp3
+/home/faizal/Music/Justin Bieber - Love Me (Official Music Video).mp3
+/home/faizal/Music/Justin Bieber - Favorite Girl -.mp3
+/home/faizal/Music/Justin Bieber - Be Alright (Audio).mp3
+/home/faizal/Music/Justin Bieber Ft.Sean Kingston- Eenie Meenie.mp3
+/home/faizal/Music/Ed Sheeran - Photograph (Official Music Video).mp3
+/home/faizal/Music/Even If I Die, It s You.mp3
+/home/faizal/Music/[God Eater OST] Have You Ever Seen... - Ghost Oracle Drive.mp3
+/home/faizal/Music/Yeh Un Dinon Ki Baat Hai Title Song Valentine s Week Special Kumar Sanu and Sadhana Sargam.mp3
+/home/faizal/Music/YEH UN DINON KI BAAT HAI PIANO TUTORIAL SYNTHESIA BY SOOR TAAL.mp3
+/home/faizal/Music/Ed Sheeran - Perfect.mp3
+/home/faizal/Music/Justin Bieber - One Less Lonely Girl.mp3
+/home/faizal/Music/【mew】 We ll Be Alright ║ Weathering With You OST ║ Full ENGLISH Cover & 大丈夫.mp3
+/home/faizal/Music/weathering_with_you.mp3
+/home/faizal/Music/We ll Be Alright (Movie Edit) (1).mp3
+/home/faizal/Music/Ghost in a flower A whisker away .mp3
+/home/faizal/Music/Taylor Swift - Love Story romeo save me .mp3
+/home/faizal/Music/your lie in april - opening 1 (8D Audio).mp3
+/home/faizal/Music/Justin Bieber - Love Yourself.mp3
+/home/faizal/Music/平行線 - Eve × suis from ヨルシカ MV.mp3
+/home/faizal/Music/ヒグチアイ 悪魔の子 (アニメスペシャルVer.).mp3
+/home/faizal/Music/あれくん Alekun &『ユイカ』 Yuika -「 あのね。 Ano ne. .mp3
+/home/faizal/Music/もう一度 One more time - Mosawo もさを ft. asmi .mp3
+/home/faizal/Music/BTS (방탄소년단) 'Film out' Official MV_.mp3
+/home/faizal/Music/Still With You_BksBNbTIoPE.mp3
+/home/faizal/Music/It_is_realme.mp3
+/home/faizal/Music/make_it_right_bts.mp3
+/home/faizal/Music/Holy F - ＂Tom Tom＂.mp3
+/home/faizal/Music/Gats (1).mp3
+/home/faizal/Music/Student X.mp3
+/home/faizal/Music/恋色 もさを。【Music Video】.mp3
+/home/faizal/Music/01. 月姫.flac
+/home/faizal/Music/02. おでんの愛刀.flac
+/home/faizal/Music/03. サンジVS.クイーン.flac
+/home/faizal/Music/04. ゾロVS.キング.flac
+/home/faizal/Music/05. ビッグ・マム覚醒.flac
+/home/faizal/Music/06. KROOM.flac
+/home/faizal/Music/07. キッド&ローVS.ビッグ・マム.flac
+/home/faizal/Music/08. 酒龍八卦.flac
+/home/faizal/Music/09. ルフィの死.flac
+/home/faizal/Music/10. 忍び寄るズニーシャ.flac
+/home/faizal/Music/11. ジンベエアイキャッチ.flac
+/home/faizal/Music/12. 日和の想い.flac
+/home/faizal/Music/13. ルフィ復活.flac
+/home/faizal/Music/14. 解放の戦士.flac
+/home/faizal/Music/15. おれの最高地点.flac
+/home/faizal/Music/16. 世界で最もふざけた能力.flac
+/home/faizal/Music/17. ギア5の戦い.flac
+/home/faizal/Music/18. リンリンとの別れ.flac
+/home/faizal/Music/19. ワノ国の20年.flac
+/home/faizal/Music/20. 目指すべき世界.flac
+/home/faizal/Music/21. 火事場の底力.flac
+/home/faizal/Music/22. 終結、ルフィVS.カイドウ.flac
+/home/faizal/Music/23. 龍王の最後.flac
+/home/faizal/Music/24. 火祭り～祭りは最高潮！！～.flac
+/home/faizal/Music/25. 人々の願い～平和への祈りを天へ～.flac
+/home/faizal/Music/26. ワノ国将軍 光月モモの助.flac
+/home/faizal/Music/27. 海軍大将 緑牛.flac
+/home/faizal/Music/28. 四皇 赤髪のシャンクス.flac
+/home/faizal/Music/29. 平和が戻ったワノ国.flac
+/home/faizal/Music/30. ワノ国・終幕.flac
+/home/faizal/Music/31. ルフィの夢.flac
